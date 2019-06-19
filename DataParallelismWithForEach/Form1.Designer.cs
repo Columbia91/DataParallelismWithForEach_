8dd193d517d5c79a4dd776ff25180299fafa41e2
ADILET <alpha.z91@mail.ru>
2019-06-19 16:31:55 +0600

    Добавьте файлы проекта.

---
+++ DataParallelismWithForEach/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace DataParallelismWithForEach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcessImage = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnProcessImage
+            // 
+            this.btnProcessImage.Location = new System.Drawing.Point(382, 106);
+            this.btnProcessImage.Name = "btnProcessImage";
+            this.btnProcessImage.Size = new System.Drawing.Size(75, 23);
+            this.btnProcessImage.TabIndex = 0;
+            this.btnProcessImage.Text = "button1";
+            this.btnProcessImage.UseVisualStyleBackColor = true;
+            this.btnProcessImage.Click += new System.EventHandler(this.btnProcessImage_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 107);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(161, 192);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnProcessImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcessImage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ DataParallelismWithForEach/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataParallelismWithForEach
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource cancelToken = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcessImage_Click(object sender, EventArgs e)
+        {
+            // Запустить новую "задачу" для обработки файлов.
+            Task.Factory.StartNew(() =>
+            {
+                ProcessFiles();
+            });
+        }
+        private void ProcessFiles()
+        {
+            // Использовать экземпляр ParallelOptions для хранения CancellationToken.
+            ParallelOptions parOpts = new ParallelOptions();
+            parOpts.CancellationToken = cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = System.Environment.ProcessorCount;
+            // Загрузить все файлы *.jpg и создать новую папку для модифицированных данных,
+            string[] files = Directory.GetFiles
+            (@"C:\Users\Nurdaulet\Pictures\Raptr Screenshots", "*.jpg",
+            SearchOption.AllDirectories);
+            string newDir = @"C:\ModifiedPictures";
+            Directory.CreateDirectory(newDir);
+            try
+            {
+                // Обработать данные изображения в параллельном режиме!
+                Parallel.ForEach(files, parOpts, currentFile =>
+                {
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
+                    string filename = Path.GetFileName(currentFile);
+                    using (Bitmap bitmap = new Bitmap(currentFile))
+                    {
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(Path.Combine(newDir, filename));
+                        this.Invoke((Action)delegate
+                        {
+                            this.Text = string.Format("Processing {0} on thread {1}", filename,
+                            Thread.CurrentThread.ManagedThreadId);
+                        }
+                        );
+                    }
+                });
+            }
+            catch (OperationCanceledException ex)
+            {
+                this.Invoke((Action)delegate
+                {
+                    this.Text = ex.Message;
+                });
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cancelToken.Cancel();
+        }
+    }
+}
